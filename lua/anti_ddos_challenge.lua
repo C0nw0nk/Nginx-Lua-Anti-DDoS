7923cf0cfef626a5513a3ba11017b9ae52b5d305
Conor McKnight <12204587+C0nw0nk@users.noreply.github.com>
2025-09-19 07:34:35 +0100

    Update anti_ddos_challenge.lua
    Only needs to be a head request method don't need any of the others.

--- lua/anti_ddos_challenge.lua
+++ lua/anti_ddos_challenge.lua
@@ -1938,7 +1938,8 @@ local function get_resp_content_type(forced) --incase content-type header not ye
 		CONNECT = localized.ngx_HTTP_CONNECT, --does not exist but put here never know in the future
 	}
 	local res = localized.ngx.location.capture(localized.request_uri, {
-	method = map[localized.ngx_var.request_method],
+	--method = map[localized.ngx_var.request_method],
+	method = map[HEAD], --only need head to grab headers dont need contents
 	--headers = req_headers,
 	})
 	if res then
@@ -6371,7 +6372,7 @@ local function minification(content_type_list)
 				For debugging tests i have checked these and they work fine i am leaving this here for future refrence
 				curl post request test - curl.exe "http://localhost/" -H "User-Agent: testagent" -H "Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8" -H "Accept-Language: en-GB,en;q=0.5" -H "Accept-Encoding: gzip, deflate, br, zstd" -H "DNT: 1" -H "Connection: keep-alive" -H "Cookie: name1=1; name2=2; logged_in=1" -H "Upgrade-Insecure-Requests: 1" -H "Sec-Fetch-Dest: document" -H "Sec-Fetch-Mode: navigate" -H "Sec-Fetch-Site: none" -H "Sec-Fetch-User: ?1" -H "Priority: u=0, i" -H "Pragma: no-cache" -H "Cache-Control: no-cache" --request POST --data '{"username":"xyz","password":"xyz"}' -H "Content-Type: application/json"
 				curl post no data test - curl.exe "http://localhost/" -H "User-Agent: testagent" -H "Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8" -H "Accept-Language: en-GB,en;q=0.5" -H "Accept-Encoding: gzip, deflate, br, zstd" -H "DNT: 1" -H "Connection: keep-alive" -H "Cookie: name1=1; name2=2; logged_in=1" -H "Upgrade-Insecure-Requests: 1" -H "Sec-Fetch-Dest: document" -H "Sec-Fetch-Mode: navigate" -H "Sec-Fetch-Site: none" -H "Sec-Fetch-User: ?1" -H "Priority: u=0, i" -H "Pragma: no-cache" -H "Cache-Control: no-cache" --request POST -H "Content-Type: application/json"
-				
+
 				client_body_in_file_only on; #nginx config to test / debug on post data being stored in file incase of large post data sizes the nginx memory buffer was not big enough i turned this on to check this works as it should.
 				]]
 				localized.ngx_req_read_body()
